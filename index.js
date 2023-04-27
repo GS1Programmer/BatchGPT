ffe107b3f68387ee9d9fca7ec20a92e40413708f
GS1Programmer <hilmanr.d9@gmail.com>
2023-04-27 15:22:26 +0700

    index file

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("close (Suggested)/ctrl + C (Windows) to exit conversation and chat");
+let conversationLog = [{ role: 'system', content: 'You are a friendly chatbot.' }];
+
+(() => {
+    const { OpenAIApi, Configuration } = require('openai')
+    const prompt = require('prompt-sync')();
+
+    const chat = async () => {
+        const configuration = new Configuration({
+            apiKey: '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',
+        })
+        const openai = new OpenAIApi(configuration);
+
+        const GPTquestion = prompt("Question : ")
+
+        conversationLog.reverse();
+        conversationLog.push({
+            role: 'user',
+            content: GPTquestion
+        });
+        
+        try {
+            const result = (await openai
+                .createChatCompletion({
+                    model: "gpt-3.5-turbo",
+                    messages: conversationLog,
+            })).data.choices[0].message.content;
+
+            console.log(`\n${result}\n====================`)
+        } catch (error) {
+            console.log(`\nERR: ${error}\n====================`);
+        }
+
+        chat();
+    }
+    
+    chat();
+})()
